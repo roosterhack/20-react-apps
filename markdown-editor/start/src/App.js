30677684002aafc8caedb4c90567c3f5f22f5639
eddie chung <eddie.chung@moonfare.com>
2020-04-23 22:01:15 +0200

    markdown editor

--- markdown-editor/start/src/App.js
+++ markdown-editor/start/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import "./App.css";
 
 export default function App() {
+  const [markdown, setMarkdown] = useState("# sup");
+
   return (
     <div className="app">
-      <textarea />
-
-      <div className="preview" />
+      <textarea
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
+      />
+      {/* 
+      <div
+        className="preview"
+        dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+      /> */}
+      <ReactMarkdown className="preview" source={markdown} />
     </div>
   );
 }
