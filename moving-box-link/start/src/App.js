949a3ff0c022f8f60f42702dcc1b71438da4abbf
Eddie Chung <designered@gmail.com>
2020-04-26 21:22:49 +0200

    add box moving logic

--- moving-box-link/start/src/App.js
+++ moving-box-link/start/src/App.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './App.css';
 
 export default function App() {
-  return (
-    <div className="app">
-      <canvas />
+  const canvasRef = useRef(null);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
 
-      <div className="arrows">
-        <button>Up</button>
-        <button>Left</button>
-        <button>Down</button>
-        <button>Right</button>
+  useEffect(() => {
+    const context = canvasRef.current.getContext('2d');
+    context.canvas.height = window.innerHeight;
+    context.canvas.width = window.innerWidth;
+
+    context.fillRect(x, y, 100, 100);
+  }, []);
+
+  //move the boxx if 0 or y changes
+  useEffect(() => {
+    const context = canvasRef.current.getContext('2d');
+    context.clearRect(0, 0, window.innerHeight, window.innerWidth);
+    context.fillRect(x, y, 100, 100);
+  }, [x, y]);
+
+  return (
+    <div className='app'>
+      <canvas ref={canvasRef} />
+
+      <div className='arrows'>
+        <button onClick={() => setY((Y) => Y - 20)}>Up</button>
+        <button onClick={() => setX((x) => x - 20)}>Left</button>
+        <button onClick={() => setY((Y) => Y + 20)}>Down</button>
+        <button onClick={() => setX((x) => x + 20)}>Right</button>
       </div>
 
-      <div className="images">
-        <img src="https://i.imgur.com/JYUB0m3.png" alt="Down" />
-        <img src="https://i.imgur.com/GEXD7bk.gif" alt="Right" />
-        <img src="https://i.imgur.com/XSA2Oom.gif" alt="Up" />
-        <img src="https://i.imgur.com/4LGAZ8t.gif" alt="Left" />
+      <div className='images'>
+        <img src='https://i.imgur.com/JYUB0m3.png' alt='Down' />
+        <img src='https://i.imgur.com/GEXD7bk.gif' alt='Right' />
+        <img src='https://i.imgur.com/XSA2Oom.gif' alt='Up' />
+        <img src='https://i.imgur.com/4LGAZ8t.gif' alt='Left' />
       </div>
     </div>
   );
