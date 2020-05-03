e125624b8c4f32d0918e23359d3f3b3557480504
eddie chung <eddie.chung@moonfare.com>
2020-05-03 18:22:53 +0200

    get and serch photos from api

--- image-gallery/start/src/App.js
+++ image-gallery/start/src/App.js
@@ -1,22 +1,44 @@
 import React, { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import "./App.css";
 
 export default function App() {
   const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
   const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
-    const fetchImages = async () => {
-      const res = await fetch(
-        `https://api.unsplash.com/photos/?client_id=${accessKey}`
-      );
-      if (res) {
-        const data = await res.json();
-        setImages(data);
-      }
-    };
     fetchImages();
-  }, [accessKey]);
+    // eslint-disable-next-line
+  }, [page]);
+
+  const fetchImages = async () => {
+    let apiUrl = `https://api.unsplash.com/photos?`;
+
+    if (query) apiUrl = `https://api.unsplash.com/search/photos?query=${query}`;
+
+    apiUrl += `&page=${page}`;
+    apiUrl += `&client_id=${accessKey}`;
+
+    const res = await fetch(apiUrl);
+    if (res) {
+      const data = await res.json();
+      const imagesFromApi = data.results ?? data;
+
+      //if page is 1, then we need a whole new array of images
+      if (page === 1) setImages(imagesFromApi);
+
+      //if page > 1, then we are adding for our infinite scroll
+      setImages((images) => [...images, ...imagesFromApi]);
+    }
+  };
+
+  const searchPhotos = (e) => {
+    e.preventDefault();
+    setPage(1);
+    fetchImages();
+  };
 
   if (!accessKey) {
     return (
@@ -30,18 +52,36 @@ export default function App() {
     <div className="app">
       <h1>Unsplash Image Gallery!</h1>
 
-      <form>
-        <input type="text" placeholder="Search Unsplash..." />
+      <form onSubmit={searchPhotos}>
+        <input
+          type="text"
+          placeholder="Search Unsplash..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
         <button>Search</button>
       </form>
 
-      <div className="image-grid">
-        {images.map((image, index) => (
-          <div className="image" key={index}>
-            <img src={image.urls.regular} alt={image.alt_description} />
-          </div>
-        ))}
-      </div>
+      <InfiniteScroll
+        dataLength={images.length}
+        next={() => setPage((page) => page + 1)}
+        hasMore
+        loader={<h4>Loading...</h4>}
+      >
+        <div className="image-grid">
+          {images.map((image, index) => (
+            <a
+              href={image.links.html}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="image"
+              key={index}
+            >
+              <img src={image.urls.regular} alt={image.alt_description} />
+            </a>
+          ))}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 }
