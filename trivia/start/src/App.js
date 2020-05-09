e5e889c6f20e61fbbe57d1d044a9e1c31073dbe3
Eddie Chung <designered@gmail.com>
2020-05-09 13:34:38 +0200

    add show selected results logic

--- trivia/start/src/App.js
+++ trivia/start/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import Question from './components/Question';
 import CategorySelector from './components/CategorySelector';
 import ResultModal from './components/ResultModal';
@@ -8,28 +8,42 @@ import './App.css';
 export default function App() {
   const [question, setQuestion] = useState(null);
   const [selectedCategory, setSelectedCategory] = useState('any');
+  const [isCorrect, setIsCorrect] = useState(false);
+
+  const getQuestion = useCallback(async () => {
+    setIsCorrect(null);
+    let url = 'https://opentdb.com/api.php?amount=1';
+    if (selectedCategory !== 'any') url += `&category=${selectedCategory}`;
+    try {
+      const res = await fetch(url);
+      const resQuestion = await res.json();
+      if (resQuestion) {
+        setQuestion(resQuestion.results[0]);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }, [selectedCategory]);
+
+  const handleQuestionAnswered = (answer) => {
+    const isAnswerCorrect = answer === question.correct_answer;
+    setIsCorrect(isAnswerCorrect);
+  };
 
   useEffect(() => {
-    const getQuestion = async () => {
-      let url = 'https://opentdb.com/api.php?amount=1';
-      if (selectedCategory !== 'any') url += `&category=${selectedCategory}`;
-      try {
-        const res = await fetch(url);
-        const resQuestion = await res.json();
-        if (resQuestion) {
-          setQuestion(resQuestion.results[0]);
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    };
     getQuestion();
-  }, [selectedCategory]);
+  }, [selectedCategory, getQuestion]);
 
   return (
     <div className='app'>
       {/* show the result modal ----------------------- */}
-      {/* <ResultModal /> */}
+      {isCorrect !== null && question && (
+        <ResultModal
+          isCorrect={isCorrect}
+          question={question}
+          getQuestion={getQuestion}
+        />
+      )}
 
       {/* question header ----------------------- */}
       <div className='question-header'>
@@ -42,12 +56,17 @@ export default function App() {
 
       {/* the question itself ----------------------- */}
       <div className='question-main'>
-        {question && <Question question={question} />}
+        {question && (
+          <Question
+            question={question}
+            answerQuestion={handleQuestionAnswered}
+          />
+        )}
       </div>
 
       {/* question footer ----------------------- */}
       <div className='question-footer'>
-        <button>Go to next question 👉</button>
+        <button onClick={getQuestion}>Go to next question 👉</button>
       </div>
     </div>
   );
--- trivia/start/src/components/ResultModal.js
+++ trivia/start/src/components/ResultModal.js
@@ -1,29 +1,38 @@
 import React from 'react';
 
-export default function ResultModal() {
+export default function ResultModal({ isCorrect, question, getQuestion }) {
   return (
-    <div className="result-modal">
-      <div className="overlay" />
-      <div className="result-modal-content">
-        <h3>
-          👊👊👊
-          <br />
-          YOU WON!
-        </h3>
+    <div className={`result-modal ${isCorrect ? 'is-correct' : 'is-wrong'}`}>
+      <div className='overlay' />
+      <div className='result-modal-content'>
+        {isCorrect && (
+          <>
+            <h3>
+              👊👊👊
+              <br />
+              YOU WON!
+            </h3>
+          </>
+        )}
+        {!isCorrect && (
+          <>
+            <h3>
+              😟😢😟
+              <br />
+              YOU LOST!
+            </h3>
+          </>
+        )}
 
-        <h3>
-          😟😢😟
-          <br />
-          YOU LOST!
-        </h3>
-
-        <div className="correct-answer">
+        <div className='correct-answer'>
           <small>The correct answer was:</small>
           <br />
-          <strong>Answer here</strong>
+          <strong
+            dangerouslySetInnerHTML={{ __html: question.correct_anwser }}
+          ></strong>
         </div>
 
-        <button>Go to next question 👉</button>
+        <button onClick={getQuestion}>Go to next question 👉</button>
       </div>
     </div>
   );
