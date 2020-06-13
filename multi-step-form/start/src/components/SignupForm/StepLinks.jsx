c0ac0ecba1b80d17600c8dc560b7a498d5a460a6
Eddie Chung <designered@gmail.com>
2020-06-13 20:26:32 +0200

    add disabled links

--- multi-step-form/start/src/components/SignupForm/StepLinks.jsx
+++ multi-step-form/start/src/components/SignupForm/StepLinks.jsx
@@ -1,14 +1,42 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSignupForm } from './SignupFormContext';
+
+const isEmpty = (obj) => Object.keys(obj).length === 0;
 
 export const StepLinks = () => {
+  const { profile, social } = useSignupForm();
+  const isProfileDone = !isEmpty(profile);
+  const isSocialDone = !isEmpty(social);
+
   return (
-    <div className="step-links">
-      <NavLink to="/" exact>
-        Profile
+    <div className='step-links'>
+      <NavLink to='/' exact>
+        {isProfileDone ? '❤️' : '💔'}Profile
+        <span />
       </NavLink>
-      <NavLink to="/social">Social</NavLink>
-      <NavLink to="/review">Review</NavLink>
+      {isProfileDone ? (
+        <NavLink to='/social'>
+          {isSocialDone ? '❤️' : '💔'}Social
+          <span />
+        </NavLink>
+      ) : (
+        <a>
+          Social
+          <span />
+        </a>
+      )}
+      {isSocialDone ? (
+        <NavLink to='/review' style={{ float: 'right ' }}>
+          Review
+          <span />
+        </NavLink>
+      ) : (
+        <a style={{ float: 'right' }}>
+          Review
+          <span />
+        </a>
+      )}
     </div>
   );
 };
