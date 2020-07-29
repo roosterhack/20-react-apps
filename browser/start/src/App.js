b2c01c9a75ba7c58e2de84a863196aea93ce9715
Eddie Chung <designered@gmail.com>
2020-07-29 22:20:26 +0200

    add address bar logic

--- browser/start/src/App.js
+++ browser/start/src/App.js
@@ -4,20 +4,49 @@ import AddressBar from './components/AddressBar';
 import './App.css';
 
 export default function App() {
-  const [browsers, setBrowser] = useState([
+  //State
+  const [browsers, setBrowsers] = useState([
     'http://eddiechungdesign.com',
     'https://learn.chrisoncode.io',
   ]);
 
   const [activeBrowser, setActiveBrowser] = useState(0);
+
+  const chooseBrowser = (id) => {
+    setActiveBrowser(id);
+  };
+
+  const addBrowser = () => {
+    const newBrowsers = [...browsers, ''];
+    setBrowsers(newBrowsers);
+    setActiveBrowser(newBrowsers.length - 1);
+  };
+
+  const updateBrowser = (url) => {
+    const newBrowsers = [...browsers];
+    newBrowsers[activeBrowser] = url;
+    setBrowsers(newBrowsers);
+  };
+
   const url = browsers[activeBrowser];
+
+  // Effects
+
+  //Functions here
+
+  //Formatting or grabbing of data
 
   return (
     <div className='app'>
       <div className='browser'>
-        <Tabs browsers={browsers} active={activeBrowser} />
+        <Tabs
+          browsers={browsers}
+          active={activeBrowser}
+          choose={chooseBrowser}
+          add={addBrowser}
+        />
 
-        <AddressBar />
+        <AddressBar update={updateBrowser} url={url} />
 
         <div className='viewport'>
           {url ? (
--- browser/start/src/components/AddressBar.js
+++ browser/start/src/components/AddressBar.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function AddressBar() {
+const addHttps = (url) => {
+  if (!url.startsWith('http') || !url.startsWith('https')) {
+    return `http://${url}`;
+  }
+  return url;
+};
+
+export default function AddressBar({ update, url }) {
+  const [value, setValue] = useState(url || '');
+
+  useEffect(() => {
+    setValue(url);
+  }, [url]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //check for https
+    const httpsUrl = addHttps(value);
+    update(httpsUrl);
+  };
+
   return (
-    <div className="address-bar">
-      <form>
-        <input type="text" name="url" />
+    <div className='address-bar'>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='url'
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+        />
       </form>
     </div>
   );
