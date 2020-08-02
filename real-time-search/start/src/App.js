fad409b0e2c82973b7bf3ddb057032afa891e7f6
Eddie Chung <designered@gmail.com>
2020-08-02 18:15:15 +0200

    e-commerce search with Agolia

--- real-time-search/start/src/App.js
+++ real-time-search/start/src/App.js
@@ -1,19 +1,55 @@
 import React from 'react';
 import './App.css';
+import algoliasearch from 'algoliasearch';
+import {
+  InstantSearch,
+  SearchBox,
+  Hits,
+  Pagination,
+} from 'react-instantsearch-dom';
 
 // ===== OPTION 1: if you want to create your own algolia account and fill it with data =====
 // demo data: https://www.algolia.com/doc/guides/building-search-ui/resources/demos/react/#media
 // sample datasets on github: https://github.com/algolia/datasets
 
 // ===== OPTION 2: if you want to get started quickly =====
-// app id: SSN9CKZUJ2
-// api key: ae8a3f7cff93d40282a4831930bfa38b
-// index name: products
+// app id: 0PZTXL7AZ6
+// api key: 961853433f9efd5ebfbff8d2d6f7cfa2
+// index name: myshop
+
+const searchClient = algoliasearch(
+  process.env.REACT_APP_ID,
+  process.env.REACT_APP_API_KEY
+);
 
 export default function App() {
   return (
-    <div className="app">
-      <div className="search-container"></div>
-    </div>
+    <InstantSearch searchClient={searchClient} indexName='myshop'>
+      <div className='app'>
+        <div className='search-container'>
+          <SearchBox />
+          <Hits hitComponent={Product} />
+          <Pagination />
+        </div>
+      </div>
+    </InstantSearch>
   );
 }
+
+const Product = ({ hit }) => {
+  return (
+    <a
+      href={hit.url}
+      className='product'
+      target='_blank'
+      rel='noopener noreferrer'
+    >
+      <img src={hit.image} alt={hit.name} />
+      <div>
+        <h3>{hit.brand}</h3>
+        <h2>{hit.name}</h2>
+        <p>{hit.price}</p>
+      </div>
+    </a>
+  );
+};
