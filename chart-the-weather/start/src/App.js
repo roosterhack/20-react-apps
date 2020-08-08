e576649f6373cf1f8d15770988f05af0bb9c04e7
Eddie Chung <designered@gmail.com>
2020-08-09 00:12:50 +0200

    add chart

--- chart-the-weather/start/src/App.js
+++ chart-the-weather/start/src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
 import { GeoForm } from './components/GeoForm';
+import { Bar, defaults } from 'react-chartjs-2';
 
 // openweathermap.org
 // https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minutely&units=imperial&appid=yourapikey
-// api key: 
+// api key:
 
+defaults.global.legend.display = false;
 
 export default function App() {
   const [latLng, setLatLng] = useState(null);
@@ -24,6 +26,58 @@ export default function App() {
 // all things with address + lat+long go into a component
 
 // all things that deal with weather api and formatting / deisplaying weather data go into a component
+
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+const labels = [...Array(7)].map((_, i) => {
+  const date = new Date();
+  date.setDate(date.getDate() + i);
+  return days[date.getDay()];
+});
+
 const WeatherChart = () => {
-  return <div>Weather chart</div>;
+  return (
+    <Bar
+      options={{
+        tooltips: { mode: 'index', interset: false },
+        scales: {
+          xAxes: [
+            {
+              gridLines: false,
+              ticks: { fontColor: '#F680BC', fontSize: 10, padding: 20 },
+            },
+          ],
+          yAxes: [
+            {
+              gridLines: false,
+              ticks: { fontColor: '#F680BC', fontSize: 10, padding: 20 },
+            },
+          ],
+        },
+      }}
+      data={{
+        labels,
+        datasets: [
+          {
+            label: 'Hoghs',
+            backgroundColor: '#EC9CAD',
+            data: [140, 200, 300],
+          },
+          {
+            label: 'Hoghs',
+            backgroundColor: '#EC9CAD',
+            data: [40, 200, 300],
+          },
+        ],
+      }}
+    />
+  );
 };
